0f61931ec16ccc132625819078aae51363ac743c
rahulsd380 <rahulsd380@gmail.com>
2024-04-11 21:46:50 +0600

    contact us modal responsiveness created

--- src/Components/ContactUs/ContactUs.tsx
+++ src/Components/ContactUs/ContactUs.tsx
@@ -37,21 +37,21 @@ const ContactUs = ({
   return (
     <>
       {isModalOpen && (
-        <div className="bg-primary-100 bg-opacity-30 backdrop-blur-sm fixed inset-0 flex justify-center items-center z-50 w-full mx-auto">
+        <div className="bg-gray-800 bg-opacity-30  fixed inset-0 flex justify-center items-center z-50 w-full mx-auto">
           {/* Form */}
-          <div id="closeModal" className="w-5/6 mx-auto">
-            <div className="bg-blue-500 w-full p-3 rounded-t-3xl relative">
+          <div id="closeModal" className="w-5/6 mx-auto overflow-y-auto">
+            <div className="bg-gradient-to-r from-sky-500 to-blue-700 w-full p-3 rounded-t-3xl relative">
               <h1 className="text-white font-Roboto font-bold text-xl text-center">
                 Reach Out to Us Today for Any Inquiries or Assistance!
               </h1>
 
               <RxCross1
                 onClick={() => setIsModalOpen(false)}
-                className="text-white text-2xl absolute top-3 right-5 cursor-pointer"
+                className="text-white text-2xl absolute top-7 md:top-3 right-5 cursor-pointer"
               />
             </div>
-            <div className="w-full shadow-lg bg-white mb-3 border-t border-gray-100 flex gap-5 p-4">
-              <div className="w-1/2 flex flex-col gap-8 border-r-2 border-dotted items-center">
+            <div className="w-full shadow-lg bg-white rounded-b-3xl flex flex-col lg:flex-row justify-center items-center gap-5 p-4">
+              <div className="w-full max-w-[600px] lg:w-1/2 flex flex-col gap-8 border-b-2 lg:border-b-0 border-r-0 lg:border-r-2 pb-4 lg:pb-0 border-dotted items-center">
                 <div className="flex flex-col gap-1">
                   <h1 className="text-xl font-Roboto font-semibold">
                     Let's Start a Conversation About Your Needs and Queries!
@@ -68,15 +68,33 @@ const ContactUs = ({
                   </h1>
 
                   <div className="flex gap-5 mt-3">
-                    <div className="relative">
+                    {/* <div className="relative">
                       <div className="bg-gray-500 w-[1px] h-[90px] absolute mt-2 ml-2 z-10"></div>
                       <div className="flex flex-col gap-6 z-20">
                         <VscCallOutgoing className="text-xl z-20 bg-white" />
                         <HiOutlineUserGroup className="text-xl z-20 bg-white" />
                         <LuUserCheck className="text-xl z-20 bg-white" />
                       </div>
-                    </div>
+                    </div> */}
 
+                    <div className="flex flex-col gap-5 relative">
+                    <div className="bg-gray-500 w-[1px] h-[130px] md:h-[90px] absolute mt-5 md:mt-2 ml-2 z-10"></div>
+                      <p className="text-gray-800 font-Roboto flex items-center gap-4">
+                      <VscCallOutgoing className="text-xl z-20 bg-white" /> Once you share your issue, our experts will reach you
+                        out shortly.
+                      </p>
+                      <p className="text-gray-800 font-Roboto flex items-center gap-4">
+                        {" "}
+                        <HiOutlineUserGroup className="text-xl z-20 bg-white" /> Once you share your issue, our experts will reach you
+                        out shortly.
+                      </p>
+                      <p className="text-gray-800 font-Roboto flex items-center gap-4">
+                        {" "}
+                        <LuUserCheck className="text-xl z-20 bg-white" /> Once you share your issue, our experts will reach you
+                        out shortly.
+                      </p>
+                    </div>
+{/* 
                     <div className="flex flex-col gap-5">
                       <p className="text-gray-800 font-Roboto">
                         Once you share your issue, our experts will reach you
@@ -92,10 +110,10 @@ const ContactUs = ({
                         Once you share your issue, our experts will reach you
                         out shortly.
                       </p>
-                    </div>
+                    </div> */}
                   </div>
 
-                  <div className="p-5 w-full rounded bg-gray-100 flex flex-col md:flex-row gap-10 md:gap-0 items-center justify-between mt-5">
+                  <div className="p-5 w-full rounded-lg bg-gradient-to-r from-gray-50 to-gray-100 flex flex-col md:flex-row gap-10 md:gap-0 items-center justify-between mt-5">
                     <div className="flex flex-col gap-1 items-center">
                       <div className="rounded-full w-20 h-20 p-2 bg-white flex justify-center items-center">
                         <FiMessageSquare className="text-5xl text-blue-400" />
@@ -126,7 +144,7 @@ const ContactUs = ({
                 </div>
               </div>
 
-              <form className="flex flex-col gap-3 bg-white rounded-3xl w-1/2">
+              <form className="flex flex-col gap-3 bg-white rounded-3xl w-full max-w-[600px] lg:w-1/2">
                 <input
                   type="text"
                   id="patientName"
